574f10e0b8a43218e1598ead63856108d8aaaa5d
Jared Ruiz <jared707@gmail.com>
2022-06-05 23:02:38 -0500

    Added thought model alongside the reaction schema to be nested within the thought model. Getters added as well.

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types } = require('mongoose');
+//getter from utils folder. Formats date to a presentable 
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default:  Date.now,
+        get: createdAt => dateFormat(createdAt)
+    }
+},
+{
+    toJSON: {
+        getters: true
+    }
+})
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        //adds a min of 1 character to max of 280 for string requirement
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //this getter should take in default date and format it at the least
+        get: createdAt => dateFormat(createdAt)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    //array of reaction objects
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    }
+})
+
+//virtual that retrieves length of thought's reaction array
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.reduce((total, reactions) => total + reactions.length + 1, 0)
+})
+
+//create thought model
+const Thought = model('Thought', thoughtSchema);
+
+//export thought model to index
+module.exports = Thought;
