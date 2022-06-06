e8c98e20abb50554881436d98ef3186cdd890f3b
Jared Ruiz <jared707@gmail.com>
2022-06-05 21:37:22 -0500

    Added starter server code. Server starts up successfully. Commented out routes until we set up routes later.

---
+++ server.js
@@ -0,0 +1,23 @@
+//require modules
+const mongoose = require('mongoose');
+const express = require('express');
+
+//general express set up
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//utilize established api routes (create them and export to use)
+// app.use(require('./routes'));
+
+//mongoose connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+//server listener
+app.listen(PORT, () => console.log(`You are successfully connected to localhost: ${PORT}`));
